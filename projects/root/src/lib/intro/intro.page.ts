11d0d7ff0e8a144a793a0f40f7c60ce692db3a1f
tanbbang01 <tanbbang01@gmail.com>
2024-11-12 08:48:09 +0900

    Move about page to Intro, add Animation when scroll down

--- projects/root/src/lib/intro/intro.page.ts
+++ projects/root/src/lib/intro/intro.page.ts
@@ -1,32 +1,73 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, NgZone, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { MessageService } from '../../../../messages/src/public-api';
+import { Subscription } from 'rxjs';
+import { WindowInfo, WindowService } from '../../../../common/src/public-api';
+
+const fragments = ['section1', 'section2', 'section3', 'section4', 'section5', 'section6', 'section7'];
 
 @Component({
   templateUrl: './intro.page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None
 })
-export class IntroPage {
+export class IntroPage implements OnInit {
+  // @ViewChild('top', { read: ElementRef }) topEl: ElementRef;
+  windowInfo: WindowInfo = new WindowInfo();
+  sectionHeight = 860; // SECTION기본높이
+  frag = 1;
+  useFrag: boolean = false;
+  isHeaderFixed = false;
+  fixedHeaderHeight = 140;
+  fixedHeaderOffset = this.fixedHeaderHeight;
+  pageOffset: number = 0;
+  scrollButtonBottom = 50;
+
+  // private windowInfoSubscription: Subscription;
+
   constructor(
     private router: Router,
-    private messageService: MessageService
-  ) { }
+    private cdr: ChangeDetectorRef,
+    private ngZone: NgZone,
+    private messageService: MessageService,
+    private windowService: WindowService,
+  ) {
+    // 스크롤이벤트발생시, 화면에 보이는 fragment section 찾기
+    window.addEventListener('scroll', () => this.checkIfFragmentsVisible());
+  }
 
-  openDialog(dialogType: string) {
-    switch (dialogType) {
-      case 'alert':
-        this.messageService.alert($localize`This is an alert message.`, $localize`Alert`);
-        break;
-      case 'confirm':
-        this.messageService.confirm($localize`Confirm`, $localize`Are you sure?`, (yes: boolean) => {
-          if (yes) this.messageService.toastMessage($localize`Confirmed`);
-          else this.messageService.toastMessage($localize`Rejected`);
-        });
-        break;
-      case 'toast':
-        this.messageService.toastMessage($localize`This is a Toast Message`);
-        break;
-    }
+  ngOnInit(): void {
+    this.windowService.getWindowInfo$().subscribe((info: WindowInfo) => {
+      this.windowInfo = info;
+      this.useFrag = (this.windowInfo.isLargeScreen || this.windowInfo.isVeryLargeScreen) ? true : false;
+      this.fixedHeaderOffset = this.windowInfo.isMediumScreen ? 115 : this.fixedHeaderHeight;
+      if (this.useFrag && this.windowInfo.clientHeight > 860)
+        this.sectionHeight = this.windowInfo.clientHeight - this.fixedHeaderHeight;
+
+      this.pageOffset = this.windowInfo.verticalOffset;
+      this.isHeaderFixed = this.pageOffset > this.fixedHeaderOffset ? true : false;
+
+      if (this.windowInfo.clientHeight !== this.windowInfo.scrollHeight && this.windowInfo.clientHeight === this.windowInfo.scrollHeight - this.pageOffset) {
+        this.scrollButtonBottom = 150;
+      } else {
+        this.scrollButtonBottom = 50;
+      }
+
+      this.cdr.detectChanges();
+    });
+  }
+
+  /**
+   * 화면에 보이는 section 찾기
+   */
+  private checkIfFragmentsVisible() {
+    setTimeout(() => {
+      fragments.forEach((fragment: string) => {
+        const el = document.getElementById(fragment);
+        if (el && this.windowService.isVisible(el)) {
+          this.frag = Number(fragment.substring(7, 8));
+        }
+      });
+    }, 100);
   }
 }
